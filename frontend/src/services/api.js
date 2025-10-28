66df737659acf3d2b7bb6909c808b03d8e0cac03
PrathapJayasankar <prathapjaya373@gmail.com>
2025-10-28 16:14:17 +0530

    feat: Configure frontend for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,25 +1,20 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+// When deployed on Vercel, it will use the REACT_APP_API_URL we provide.
+// When running locally (`npm start`), it will fall back to the localhost address.
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
     baseURL: API_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+    headers: { 'Content-Type': 'application/json' },
 });
 
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers['x-auth-token'] = token;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers['x-auth-token'] = token;
     }
-);
+    return config;
+}, (error) => Promise.reject(error));
 
 export default api;
